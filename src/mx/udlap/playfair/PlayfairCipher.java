f297d0fdcccdbe062a2016d8de2b3d1b3f549469
Ulysses <Ulysses@140.148.204.99>
2014-02-24 16:54:31 -0600

    Method signatures

--- src/mx/udlap/playfair/PlayfairCipher.java
+++ src/mx/udlap/playfair/PlayfairCipher.java
@@ -6,9 +6,33 @@ public class PlayfairCipher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-	//Prueba
-
+	
+	/*Recibir la keyword y checar las letras repetidas*/
+	public static String getKeyword(){
+		return "";
+	}
+	
+	/*Separar el plaintext en diagrams.
+	 * Si hay letras repetidas, insertar x.
+	 * Si el numero de letras es impar, agregar x al final.
+	 */
+	public static String textToDigrams(String plainText){
+		return "";
+	}
+	
+	/*Crear la matriz y poner la keyword en la matriz.
+	 * Terminar de llenar la matriz, sin contar las letras que ya están.
+	 */
+	public static char[][] createMatrix(String keyword){
+		char matrix[][] = new char[5][5];
+		
+		return matrix;
+	}
+	
+	public String cipher(char[][] matrix, String digramText){
+		return "";
+	}
+	
 }
